c48c4dfc3e82de587aa09dbe639ca473bf185caf
Jake Watson <jakejakewatsonwatson@gmail.com>
2018-08-09 12:24:14 -0500

    Stubbed out methods for calculator

--- ConsoleApp24/Calculator.cs
+++ ConsoleApp24/Calculator.cs
@@ -6,5 +6,26 @@ namespace ConsoleApp24
 {
     class Calculator
     {
+        public int Add(int a, int b)
+        {
+            return 0;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return 0;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return 0;
+        }
+
+        public int Divide(int a, int b)
+        {
+            return 0;
+        }
+
+
     }
 }
